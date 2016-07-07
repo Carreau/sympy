d2d1ed4c06cc06acff7c66b3b74e7ef8e688ffd7
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-07-07 14:27:22 -0700

    debug travis II

--- sympy/utilities/runtests.py
+++ sympy/utilities/runtests.py
@@ -493,7 +493,6 @@ def _test(*paths, **kwargs):
     # Show deprecation warnings
     import warnings
     warnings.simplefilter("error", SymPyDeprecationWarning)
-    warnings.filterwarnings('error', '.*', DeprecationWarning, module='sympy.*')
 
     test_files = t.get_test_files('sympy')
 
@@ -679,7 +678,6 @@ def _doctest(*paths, **kwargs):
     # Show deprecation warnings
     import warnings
     warnings.simplefilter("error", SymPyDeprecationWarning)
-    warnings.filterwarnings('error', '.*', DeprecationWarning, module='sympy.*')
 
     r = PyTestReporter(verbose, split=split, colors=colors,\
                        force_colors=force_colors)
